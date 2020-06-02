c10e786e33fb9c09fe1327cd9fd46ade2841d5a1
shoty-de <66251668+shoty-de@users.noreply.github.com>
2020-06-02 21:35:58 +0900

    Update article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,10 @@
 class Article < ApplicationRecord
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
   validates_presence_of(:title)
 =======
+>>>>>>> Stashed changes
+=======
+  validates :title, presence: true
 >>>>>>> Stashed changes
 end
